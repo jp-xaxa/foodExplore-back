24f719109ab05b5e28e81c21dc1f6fc1d324005e
Joao Pedro <joaopedrolamim@yahoo.com.br>
2024-06-24 13:23:51 -0300

    Created system routes

--- src/server.js
+++ src/server.js
@@ -5,11 +5,13 @@ const AppError = require("./utils/AppError")
 
 const express = require("express")
 const cors = require("cors")
+const routes = require("./routes")
 
 const app = express()
+
 app.use(cors())
 app.use(express.json())
-
+app.use(routes)
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({
