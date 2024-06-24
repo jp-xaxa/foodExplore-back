572840df8c94736e31bc0c0adcf7b4ab961cc145
Joao Pedro <joaopedrolamim@yahoo.com.br>
2024-06-24 13:21:55 -0300

    Created system apperror

--- src/server.js
+++ src/server.js
@@ -1,12 +1,30 @@
 require("express-async-errors")
 require("dotenv/config")
 
+const AppError = require("./utils/AppError")
+
 const express = require("express")
 const cors = require("cors")
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    })
+  }
+
+  console.error(error)
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal error server",
+  })
+})
 
 const PORT = process.env.PORT || 3333
 app.listen(PORT, () => {
